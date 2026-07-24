ee261dfb9e1a590c0da0cd2387b790d8aaa1241a
Lumas <lumastech@gmail.com>
2026-07-24 21:10:37 +0200

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
         // $this->call(LeaveTypesSeeder::class);
         $this->call(MenuSeeder::class);
 
